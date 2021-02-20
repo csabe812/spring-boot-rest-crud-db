e0adeac91ba3f1a7b1b4ed0cc1c0c190faed5d26
csabe812 <csafulop@gmail.com>
2021-02-20 19:29:55 +0100

    Initialized springboot project using dependencies

---
+++ src/main/java/com/github/csabe812/springbootrestcruddb/SpringBootRestCrudDbApplication.java
@@ -0,0 +1,13 @@
+package com.github.csabe812.springbootrestcruddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestCrudDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestCrudDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/csabe812/springbootrestcruddb/SpringBootRestCrudDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.csabe812.springbootrestcruddb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestCrudDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
